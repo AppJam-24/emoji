472eaf292d21ed3704fae5809e44bfaa391e64d3
JangWoojun <woojun0107@naver.com>
2024-01-06 17:47:43 +0900

    feat: binding 세팅

--- app/src/main/java/com/woojun/emoji/activity/MainActivity.kt
+++ app/src/main/java/com/woojun/emoji/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.woojun.emoji.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.woojun.emoji.R
+import com.woojun.emoji.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+        }
     }
 }
