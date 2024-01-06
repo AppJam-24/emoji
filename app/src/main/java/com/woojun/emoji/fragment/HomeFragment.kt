c30eb95b5cb8c5952aa7cb859bfa80eb5d7fc64e
JangWoojun <woojun0107@naver.com>
2024-01-06 19:20:32 +0900

    feat: helloText 일부 텍스트 색 지정

--- app/src/main/java/com/woojun/emoji/fragment/HomeFragment.kt
+++ app/src/main/java/com/woojun/emoji/fragment/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.woojun.emoji.fragment
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.woojun.emoji.R
 import com.woojun.emoji.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -27,6 +32,21 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
+            val text = helloText.text
+            val spannableString = SpannableString(text)
+            val color = ContextCompat.getColor(requireContext(), R.color.primary)
+
+            val endIndex = text.indexOf("님")
+            if (endIndex != -1) {
+                spannableString.setSpan(
+                    ForegroundColorSpan(color),
+                    0,
+                    endIndex,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+
+            helloText.text = spannableString
 
         }
     }
