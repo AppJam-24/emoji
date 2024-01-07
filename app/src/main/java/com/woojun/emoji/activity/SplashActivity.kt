56da52fa59931a787a02d3739ae2521110922bc7
JangWoojun <woojun0107@naver.com>
2024-01-07 17:59:20 +0900

    design: 스테이터스 바 삭제

--- app/src/main/java/com/woojun/emoji/activity/SplashActivity.kt
+++ app/src/main/java/com/woojun/emoji/activity/SplashActivity.kt
@@ -1,15 +1,24 @@
 package com.woojun.emoji.activity
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import com.woojun.emoji.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.apply {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
 
         Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
