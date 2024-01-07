f6b16f44ad702716fe4d132672e8a3e849e32e25
JangWoojun <woojun0107@naver.com>
2024-01-07 18:01:34 +0900

    feat: Pretrofilt.API

--- app/src/main/java/com/woojun/emoji/util/MyApp.kt
+++ app/src/main/java/com/woojun/emoji/util/MyApp.kt
@@ -4,7 +4,12 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 
 class MyApp : Application() {
+    companion object {
+        lateinit var prefs : MySharedPreferences
+    }
+
     override fun onCreate() {
+        prefs = MySharedPreferences(applicationContext)
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
