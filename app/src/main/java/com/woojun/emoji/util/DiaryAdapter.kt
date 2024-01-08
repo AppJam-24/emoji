9107e330e5d8690610f23bf2352d643960333d0a
JangWoojun <woojun0107@naver.com>
2024-01-08 20:13:29 +0900

    feat: DiaryAdapter 세팅

--- app/src/main/java/com/woojun/emoji/util/DiaryAdapter.kt
+++ app/src/main/java/com/woojun/emoji/util/DiaryAdapter.kt
@@ -1,16 +1,29 @@
 package com.woojun.emoji.util
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.woojun.emoji.R
 import com.woojun.emoji.databinding.DiaryItemBinding
 
-class DiaryAdapter(private val diaryList: List<Diary>): RecyclerView.Adapter<DiaryAdapter.DiaryViewHolder>() {
+class DiaryAdapter(private val diaryList: List<Diary>, private val context: Context): RecyclerView.Adapter<DiaryAdapter.DiaryViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiaryViewHolder {
         val binding = DiaryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return DiaryViewHolder(binding).also { handler ->
-
+            binding.apply {
+                diaryButton.setOnClickListener {
+                    val bundle = Bundle()
+                    bundle.putParcelable("diary", diaryList[handler.position])
+                    parent.findNavController().navigate(R.id.innerFragment, bundle)
+                }
+            }
         }
     }
 
@@ -19,16 +32,28 @@ class DiaryAdapter(private val diaryList: List<Diary>): RecyclerView.Adapter<Dia
     }
 
     override fun onBindViewHolder(holder: DiaryViewHolder, position: Int) {
-        holder.bind(diaryList[position])
+        holder.bind(diaryList[position], context)
     }
 
     class DiaryViewHolder(private val binding: DiaryItemBinding):
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(foodInfo: Diary) {
+        fun bind(diary: Diary, context: Context) {
             binding.apply {
-
+                title.text = diary.title
+                imageView.setImageBitmap(stringToBitmap(diary.image))
             }
         }
+        fun stringToBitmap(encodedString: String): Bitmap? {
+            return try {
+                val encodeByte = Base64.decode(encodedString, Base64.DEFAULT)
+                BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
+            } catch (e: Exception) {
+                e.message
+                null
+            }
+        }
+
     }
+
 
 }
