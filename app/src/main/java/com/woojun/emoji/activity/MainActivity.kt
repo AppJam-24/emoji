2074f508580b3e8e171b0c07c72a430f42cb0b33
JangWoojun <woojun0107@naver.com>
2024-01-08 20:11:50 +0900

    design: 스테이터스 바 제거

--- app/src/main/java/com/woojun/emoji/activity/MainActivity.kt
+++ app/src/main/java/com/woojun/emoji/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.woojun.emoji.activity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
 import com.woojun.emoji.R
 import com.woojun.emoji.databinding.ActivityMainBinding
@@ -14,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
+            window.apply {
+                decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                statusBarColor = Color.TRANSPARENT
+            }
             val navController = findNavController(R.id.nav_host_fragment)
             bottomNavigation.setItemSelected(R.id.home)
 
